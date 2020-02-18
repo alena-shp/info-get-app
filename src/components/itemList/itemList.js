289f095455c90a57f164473392088fb6682a8a44
Shpakova Alena <shpakovadev@gmail.com>
2020-02-18 14:20:46 +0300

    getData and map ItemList

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,20 +1,35 @@
 import React from "react"
 import "./itemList.scss"
+import swapiService from "../../services/swapiService"
+import Spinner from "../spinner"
 
 export default class ItemList extends React.Component {
+  swapiData = new swapiService()
+  state = {
+    peopleList: null
+  }
+
+  componentDidMount() {
+    this.swapiData.getAllPeople().then(peopleList => {
+      this.setState({ peopleList })
+      console.log(peopleList)
+    })
+  }
   render() {
-    return (
-      <ul className="itemList">
-        <li className="itemList__item">
-          <a href="#0">Luke Skywalker</a>
+    const { peopleList } = this.state
+
+    if (peopleList === null) {
+      return <Spinner />
+    }
+
+    const people = peopleList.map(person => {
+      const { id, name } = person
+      return (
+        <li key={id} className="itemList__item">
+          <a href="#0">{name}</a>
         </li>
-        <li className="itemList__item">
-          <a href="#0">Darth Vader</a>
-        </li>
-        <li className="itemList__item">
-          <a href="#0">R2-D2</a>
-        </li>
-      </ul>
-    )
+      )
+    })
+    return <ul className="itemList">{people}</ul>
   }
 }
