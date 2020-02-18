e62f3e9aef6aceba31ddf2b5904961a6c3958c3e
Shpakova Alena <shpakovadev@gmail.com>
2020-02-18 20:33:45 +0300

    add button app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,23 +7,36 @@ import PersonDetails from "../personDetails/personDetails"
 
 class App extends React.Component {
   state = {
-    
+    showrandomPlanet: true,
     numSelect: 5
   }
 
+  toggleRandomPlanet = () => {
+    this.setState(state => {
+      return {
+        showrandomPlanet: !state.showrandomPlanet
+      }
+    })
+  }
   onItemselected = id => {
     this.setState({
       numSelect: id
     })
   }
   render() {
+    const { showrandomPlanet, numSelect } = this.state
+
+    const randomPlanets = showrandomPlanet ? <RandomPlanet /> : null
     return (
       <div className="app">
         <Header />
-        <RandomPlanet />
+        {randomPlanets}
+        <button className="app__action" onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
         <div className="app__description">
           <ItemList onItemselected={this.onItemselected} />
-          <PersonDetails numSelect={this.state.numSelect}/>
+          <PersonDetails numSelect={numSelect} />
         </div>
       </div>
     )
