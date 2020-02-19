fff3b5f0f2ebe0829cd3158612687b99805feebb
Shpakova Alena <shpakovadev@gmail.com>
2020-02-19 14:19:33 +0300

    fix personDetail

--- src/components/personDetails/personDetails.js
+++ src/components/personDetails/personDetails.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./personDetails.scss"
 import swapiService from "../../services/swapiService"
+import Spinner from "../spinner"
 
 export default class PersonDetails extends React.Component {
   swapiData = new swapiService()
 
   state = {
-    person: {}
+    person: {},
+    loading: true
   }
 
   componentDidMount() {
@@ -16,6 +18,7 @@ export default class PersonDetails extends React.Component {
   componentDidUpdate(prevProps) {
     if (this.props.personId !== prevProps.personId) {
       this.updataPerson()
+      this.setState({ loading: true })
     }
   }
 
@@ -27,64 +30,77 @@ export default class PersonDetails extends React.Component {
     }
 
     this.swapiData.getPerson(personId).then(person => {
-      this.setState({ person })
-      console.log(this.state.person)
+      this.setState({ person, loading: false })
     })
   }
 
   render() {
-    const {
-      id,
-      name,
-      gender,
-      birthYear,
-      eyeColor,
-      hairColor,
-      height,
-      mass,
-      skinColor
-    } = this.state.person
+    const { person, loading } = this.state
+
+    const spinner = loading ? <Spinner /> : null
+    const content = !loading ? <ViewPerson person={person} /> : null
+
     return (
       <div className="personDetails">
-        <img
-          className="personDetails__img"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-          alt=""
-        />
-        <div className="personDetails__card card">
-          <h3 className="card__name">{name}</h3>
-          <ul className="card__description">
-            <li className="card__description-item">
-              <span>Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Birth Yea</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Hair Color</span>
-              <span>{hairColor}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Height</span>
-              <span>{height}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Mass</span>
-              <span>{mass}</span>
-            </li>
-            <li className="card__description-item">
-              <span>Skin Color</span>
-              <span>{skinColor}</span>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
     )
   }
+}
+
+const ViewPerson = ({ person }) => {
+  const {
+    id,
+    name,
+    gender,
+    birthYear,
+    eyeColor,
+    hairColor,
+    height,
+    mass,
+    skinColor
+  } = person
+  return (
+    <>
+      <img
+        className="personDetails__img"
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+        alt=""
+      />
+      <div className="personDetails__card card">
+        <h3 className="card__name">{name}</h3>
+        <ul className="card__description">
+          <li className="card__description-item">
+            <span>Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Birth Yea</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Eye Color</span>
+            <span>{eyeColor}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Hair Color</span>
+            <span>{hairColor}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Height</span>
+            <span>{height}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Mass</span>
+            <span>{mass}</span>
+          </li>
+          <li className="card__description-item">
+            <span>Skin Color</span>
+            <span>{skinColor}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
 }
