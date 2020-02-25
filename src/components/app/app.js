ed1954e8b338cd2ebad5e8fedaece8b974829f2b
Shpakova Alena <shpakovadev@gmail.com>
2020-02-25 21:16:37 +0300

    add context

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,15 +5,18 @@ import Header from "../header/header"
 import RandomPlanet from "../randomPlanet/randomPlanet"
 import ErrorBoundary from "../errorBoundary/errorBoundary"
 import Row from "../row/row"
-import ItemList from "../itemList"
 import {
   PeopleItemList,
   PlanetsItemList,
   PersonDetails,
   PlanetDetails
 } from "../wrapper-component"
+import swapiService from "./../../services/swapiService"
+import { SwapiProveder } from "./../swapiContext/swapiContext"
 
 class App extends React.Component {
+  swapiData = new swapiService()
+
   state = {
     showrandomPlanet: true
   }
@@ -27,26 +30,29 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.swapiData)
     const { showrandomPlanet } = this.state
 
     const viewRandomPlanet = showrandomPlanet ? <RandomPlanet /> : null
     return (
       <ErrorBoundary>
-        <div className="app">
-          <Header />
-          {viewRandomPlanet}
-          <button className="app__action" onClick={this.toggleRandomPlanet}>
-            Toggle Random Planet
-          </button>
-          <Row
-            left={<PeopleItemList/>}
-            right={<PersonDetails itemId="6" />}
-          />
-          <Row
-            left={<PlanetsItemList />}
-            right={<PlanetDetails itemId="4" />}
-          />
-        </div>
+        <SwapiProveder value={this.swapiData}>
+          <div className="app">
+            <Header />
+            {viewRandomPlanet}
+            <button className="app__action" onClick={this.toggleRandomPlanet}>
+              Toggle Random Planet
+            </button>
+            <Row
+              left={<PeopleItemList />}
+              right={<PersonDetails itemId="6" />}
+            />
+            <Row
+              left={<PlanetsItemList />}
+              right={<PlanetDetails itemId="4" />}
+            />
+          </div>
+        </SwapiProveder>
       </ErrorBoundary>
     )
   }
---
+++ src/components/swapiContext/swapiContext.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const {
+  Provider: SwapiProveder,
+  Consumer: SwapiConsumer
+} = React.createContext()
+
+export { SwapiProveder, SwapiConsumer }
