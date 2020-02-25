14cfad704ae73f729eb703265f870bc72c9d09bc
Shpakova Alena <shpakovadev@gmail.com>
2020-02-25 08:30:59 +0300

    add withDataDetails

---
+++ src/components/hocComponent/withDataDetails.js
@@ -0,0 +1,46 @@
+import React from "react"
+
+const WithDataDetails = View => {
+  return class extends React.Component {
+    state = {
+      item: "",
+      loading: true,
+      image: ""
+    }
+
+    componentDidMount() {
+      this.updataItem()
+    }
+
+    componentDidUpdate(prevProps) {
+      if (this.props.itemId !== prevProps.itemId) {
+        this.updataItem()
+        this.setState({ loading: true })
+      }
+    }
+
+    updataItem() {
+      const { itemId, getDetails, getImageUrl } = this.props
+
+      if (!itemId) {
+        return
+      }
+      getDetails(itemId).then(item => {
+        this.setState({ item, loading: false, image: getImageUrl(item) })
+      })
+    }
+    render() {
+      const { item, loading, image } = this.state
+      return (
+        <View
+          {...this.props}
+          item={item}
+          loading={loading}
+          image={image}
+        />
+      )
+    }
+  }
+}
+
+export default WithDataDetails
--- src/components/itemDetails/itemDetails.js
+++ src/components/itemDetails/itemDetails.js
@@ -1,64 +1,34 @@
 import React from "react"
 import "./itemDetails.scss"
-import swapiService from "../../services/swapiService"
 import Spinner from "../spinner"
+import WithDataDetails from "../hocComponent/withDataDetails"
 
-export default class ItemDetails extends React.Component {
-  swapiData = new swapiService()
+const ItemDetails = props => {
+  const { item, loading, image } = props
 
-  state = {
-    item: "",
-    loading: true,
-    image: ""
+  const { name } = item
+
+  if (item === "") {
+    return <span>Select a person from a list</span>
   }
 
-  componentDidMount() {
-    this.updataItem()
+  if (loading) {
+    return <Spinner />
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.itemId !== prevProps.itemId) {
-      this.updataItem()
-      this.setState({ loading: true })
-    }
-  }
-
-  updataItem() {
-    const { itemId, getDetails, getImageUrl } = this.props
-
-    if (!itemId) {
-      return
-    }
-    getDetails(itemId).then(item => {
-      this.setState({ item, loading: false, image: getImageUrl(item) })
-    })
-  }
-
-  render() {
-    const { item, loading, image } = this.state
-
-    const { name } = item
-
-    if (item === "") {
-      return <span>Select a person from a list</span>
-    }
-
-    if (loading) {
-      return <Spinner />
-    }
-
-    return (
-      <div className="personDetails">
-        <img className="personDetails__img" src={image} alt="" />
-        <div className="personDetails__card card">
-          <h3 className="card__name">{name}</h3>
-          <ul className="card__description">
-            {React.Children.map(this.props.children, child => {
-              return React.cloneElement(child, { item })
-            })}
-          </ul>
-        </div>
+  return (
+    <div className="personDetails">
+      <img className="personDetails__img" src={image} alt="" />
+      <div className="personDetails__card card">
+        <h3 className="card__name">{name}</h3>
+        <ul className="card__description">
+          {React.Children.map(props.children, child => {
+            return React.cloneElement(child, { item })
+          })}
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default WithDataDetails(ItemDetails)
