0b3f97408e2630bce0943786f062e9b452aea785
Shpakova Alena <shpakovadev@gmail.com>
2020-03-02 14:23:35 +0300

    add withRouter in PagePeople

--- src/components/pages/pagePeople.js
+++ src/components/pages/pagePeople.js
@@ -1,26 +1,14 @@
 import React from "react"
-import Row from "./../row/row"
-import { PeopleItemList, PersonDetails } from "../wrapper-component"
+import { withRouter } from "react-router-dom"
+import { PeopleItemList } from "../wrapper-component"
 
-export default class PagePeople extends React.Component {
-  state = {
-    itemId: ""
-  }
-
-  onItemselected = id => {
-    this.setState({
-      itemId: id
-    })
-  }
-
-  render() {
-    const { itemId } = this.state
-
-    return (
-      <Row
-        left={<PeopleItemList onItemselected={this.onItemselected} />}
-        right={<PersonDetails itemId={itemId} />}
-      />
-    )
-  }
+const PagePeople = ({ history }) => {
+  return (
+    <PeopleItemList
+      onItemselected={itemId => {
+        history.push(`/people/${itemId}`)
+      }}
+    />
+  )
 }
+export default withRouter(PagePeople)
